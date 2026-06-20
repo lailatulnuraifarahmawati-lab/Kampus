26b3c589c17b78b2f6c54117ea5960d6a1392bfa
lailatulnuraifarahmawati-lab <lailatulnuraifarahmawati@saintek.unipdu.ac.id>
2026-06-20 12:48:12 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,34 +1,3 @@
 <?php
+require_DIR_.'/../public/index.php';
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
-    $storagePath = '/tmp/storage';
-
-    foreach ([
-        $storagePath.'/framework/cache/data',
-        $storagePath.'/framework/sessions',
-        $storagePath.'/framework/views',
-        $storagePath.'/logs',
-    ] as $path) {
-        if (! is_dir($path)) {
-            mkdir($path, 0777, true);
-        }
-    }
-
-    $app->useStoragePath($storagePath);
-}
-
-$app->handleRequest(Request::capture());
